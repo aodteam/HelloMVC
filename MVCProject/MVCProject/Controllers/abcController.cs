55b1954504e5afa261e91fdb5ed7cb7af30c9705
최진혁 <aodteam@nate.com>
2017-12-12 20:26:58 +0900

    세번째 변경

---
+++ MVCProject/MVCProject/Controllers/abcController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class abcController : Controller
+    {
+        // GET: abc
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
